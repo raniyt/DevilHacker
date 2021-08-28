c3c4846cad2af91a641a2527ba817ac2a7cfbf35
raniyt <89677919+raniyt@users.noreply.github.com>
2021-08-28 10:40:33 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"✨ Check links at @TheModdingGod ✨ Found {(len_results)} Results For Your Request 😊: <code>{query}</code>",
+                text=f"✨ Check links at @RequestedMovieRani ✨ Found {(len_results)} Results For Your Request 😊: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
